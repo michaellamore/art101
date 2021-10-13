f4486e5c360301d46bd71a68893dc3db663c618a
michaellamore <michael.remorin2023@gmail.com>
2021-10-13 09:16:36 -0700

    Completed Lab 4 + Minor fixes to Homepage and Lab 3
    Lab 4:
    - Added lab partner's JS
    - Added lab partner's CSS
    - Added lab partner's HTML
    - Minor fixes to indents in HTML
    
    Homepage:
    - Every lab's CSS is affected by Homepage CSS. Made homepage CSS specific to the homepage
    - This is to get consistency between lab partner's CSS, rather than letting the homepage CSS interfere
    
    Lab 3:
    - Minor fixes to CSS, problems occurred because of the changes to homepage CSS

---
+++ lab4/js/lab.js
@@ -0,0 +1,26 @@
+//Summary. This is an outline to creating a checkers game. It goes through the
+//process of what is involved in the game like the board and the checkers
+//themselves. It also goes through the steps of playing the game too.
+//
+// Description. The game of checkers uses black and white pieces with an end
+//goal of capture all of ones opponents pieces.
+//
+// @link https://jennaadk.github.io/art101repo/
+// @file This file makes a checker game
+// @author Jenna Adkison
+// @since 10/12/2021
+
+
+// Creating a checkers game:
+// Print out a board
+// Place pieces on board
+    // 12 black pieces
+    // 12 white pieces
+// Validated users color
+// Black goes first
+// Validate users move
+    // Pieces can only move diagonally forward
+// Capture opponent's checker by jumping adjacent checkers
+// When a checker reaches the first row it becomes a king
+    // Checker can now move diagonally forward or backwards
+//First to capture all of opponents checker wins
