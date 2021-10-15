e9045a9cc572564195981b495160c7c41d95b255
michaellamore <michael.remorin2023@gmail.com>
2021-10-15 10:35:48 -0700

    Finished Lab 5
    - Added JS
    - Added HTML
    - Added CSS
    - Added img

---
+++ lab5/js/lab.js
@@ -0,0 +1,27 @@
+/*
+ * Author:    Michael Remorin <mremorin@ucsc.edu>
+ * Created:   15 October 2021
+ * License:   Public Domain
+ */
+
+// Define Variables
+let make = "Tesla";
+let model = "Model S";
+let color = "Solid Black";
+let year = 2012
+let ownIt = false;
+let dreamCar = true;
+let attainableCar = "Probably not.";
+
+// Calculate
+let age = 2021 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+document.writeln("Do you own it? " + ownIt + "<br>");
+document.writeln("Is it your dream car? " + dreamCar + "<br>");
+document.writeln("Is it an attainable car? " + attainableCar + "<br>");
